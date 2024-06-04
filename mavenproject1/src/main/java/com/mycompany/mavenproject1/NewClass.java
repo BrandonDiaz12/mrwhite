d45104c0f469bc772d05cb2e0ba907f42cd14820
Br4ndow <Br4ndow@DESKTOP-RG06U17>
2024-06-04 03:29:44 -0300

    sei la commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Br4ndow
+ */
+public class NewClass {
+    int cu = 12;
+    
+}
